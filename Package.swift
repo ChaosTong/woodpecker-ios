15720127ca9a5e7adc5aba5f4af67896f462f72c
tong chao <easyulife@gmail.com>
2025-11-27 11:18:10 +0800

    Add swiftSettings for debug configuration
    Added swiftSettings to the WoodpeckeriOS target for debug configuration.

--- Package.swift
+++ Package.swift
@@ -25,7 +25,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "WoodpeckeriOS",
-            dependencies: []),
+            dependencies: [],
+        swiftSettings: [
+    .define("DEBUG", .when(configuration: .debug))
+  ]),
         .testTarget(
             name: "WoodpeckeriOSTests",
             dependencies: ["WoodpeckeriOS"]),
